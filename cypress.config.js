ab30e8d0713b65b0d1dd4ffb5dc2f7194f8eb503
Vitor Emidio <vitorsemidio.96@gmail.com>
2023-06-08 03:42:33 -0300

    chore: enable experimentalRunAllSpecs

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost',
+    experimentalRunAllSpecs: true,
   },
   fixturesFolder: false,
   video: false,
