11bae684b4db1f56e86102613f187062c9d5246d
Vitor Emidio <vitorsemidio.96@gmail.com>
2023-06-08 03:18:28 -0300

    test: create login test

---
+++ cypress/@types/cypress.d.ts
@@ -0,0 +1,14 @@
+declare namespace Cypress {
+  interface Chainable {
+    /**
+     * Logs in to a web application using the provided credentials or the default values from environment variables.
+     *
+     * @param {string} [user] - The username for login. If not provided, the value from the 'user_name' environment variable will be used.
+     * @param {string} [password] - The password for login. If not provided, the value from the 'user_password' environment variable will be used.
+     * @example
+     * cy.login('my-username', 'my-password');
+     * cy.login(); // Uses default values from environment variables
+     */
+    login(user?: string, password?: string): void;
+  }
+}
